db2f87a6352f6011a1aacc74560d146fb1bb5807
Cozann <keislerj40@gmail.com>
2024-06-07 18:21:16 -0400

    Basic outline runs, remained files to my desired name

--- src/test/java/com/lootkeysplit/LootKeySplitPluginTest.java
+++ src/test/java/com/lootkeysplit/LootKeySplitPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.lootkeysplit;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class LootKeySplitPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(LootKeySplitPlugin.class);
 		RuneLite.main(args);
 	}
 }
